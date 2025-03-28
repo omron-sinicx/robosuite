3cb70ab0aeccdc524d43ed1e703be2f16390f22c
Cristian Beltran <3798796+cambel@users.noreply.github.com>
2025-03-28 09:53:17 +0900

    Move initial pose a bit higher

--- robosuite/environments/manipulation/osx_grind.py
+++ robosuite/environments/manipulation/osx_grind.py
@@ -758,7 +758,9 @@ class OSXGrind(ManipulationEnv):
 
         # Update the initial position of the robot based on the initial pose of the reference trajectory
         if self.reset_with_ik:
-            result = self.ik.solve_ik(target_pos=self.reference_trajectory[0][:3],
+            initial_pos = self.reference_trajectory[0][:3]
+            initial_pos[2] += 0.001
+            result = self.ik.solve_ik(target_pos=initial_pos,
                                       target_rot=T.quat2mat(self.reference_trajectory[0][3:]),
                                       initial_guess=self.init_qpos)
 
