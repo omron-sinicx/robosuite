233ea50ed2de72cad936aaff00a662c7fe5f81c7
yukitiec <kawawaki-yuki628@g.ecc.u-tokyo.ac.jp>
2025-08-19 05:42:14 +0000

    change terminal force threshold

--- robosuite/environments/manipulation/osx_grind.py
+++ robosuite/environments/manipulation/osx_grind.py
@@ -1198,7 +1198,7 @@ class OSXGrind(ManipulationEnv):
         """
         # Get the magnitude of the force (first 3 components of wrench)
         force_magnitude = np.linalg.norm(self.eef_wrench[:3])
-        return force_magnitude > 1000.0
+        return force_magnitude > 500.0
 
 
     def _check_force_torque_limits(self):
