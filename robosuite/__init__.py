0c6c4c62ef1158d05ce3c4e30a2e5b2291d8a6ad
Cristian Beltran <cristianbehe@pm.me>
2026-02-19 21:12:53 +0900

    Disable grind env

--- robosuite/__init__.py
+++ robosuite/__init__.py
@@ -14,7 +14,7 @@ from robosuite.environments.manipulation.two_arm_handover import TwoArmHandover
 from robosuite.environments.manipulation.two_arm_transport import TwoArmTransport
 from robosuite.environments.manipulation.two_arm_osx import TwoArmOSX
 from robosuite.environments.manipulation.two_arm_wiping import TwoArmWiping
-from robosuite.environments.manipulation.osx_grind import OSXGrind
+# from robosuite.environments.manipulation.osx_grind import OSXGrind
 
 from robosuite.environments import ALL_ENVIRONMENTS
 from robosuite.controllers import (
